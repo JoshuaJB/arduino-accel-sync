8aca7c1c8636cc282de8bf477e7a3b0bfa693268
fahdahmed <dahfonacob@aol.com>
2015-10-10 13:40:30 -0400

    added json object sendinga and multiple bean connection

--- iOS/Cool Beans/Controllers/ConnectedViewController.swift
+++ iOS/Cool Beans/Controllers/ConnectedViewController.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 import Foundation
+import Alamofire
+
+
+
+
 
 var someBytes = NSMutableData.init(capacity: 4);
 var byteCounter = 0;
+var concurrencyQueue:dispatch_queue_t{
+    return dispatch_get_main_queue()
+}
+
 
 struct Temperature {
     enum State {
@@ -20,26 +29,26 @@ struct Temperature {
         case Warm
         case Hot
     }
-
+    
     var degreesCelcius: Float
     var degressFahrenheit: Float {
         return (degreesCelcius * 1.8) + 32.0
     }
     
-
+    
     func state() -> State {
         let fahrenheit = degressFahrenheit
         switch Int(fahrenheit) {
-            case let x where fahrenheit <= 39:
-                return .Cold
-            case 40...65:
-                return .Cool
-            case 66...80:
-                return .Warm
-            case let x where fahrenheit >= 81:
-                return .Hot
-            default:
-                return .Unknown
+        case let x where fahrenheit <= 39:
+            return .Cold
+        case 40...65:
+            return .Cool
+        case 66...80:
+            return .Warm
+        case let x where fahrenheit >= 81:
+            return .Hot
+        default:
+            return .Unknown
         }
     }
 }
@@ -47,59 +56,87 @@ struct Temperature {
 class ConnectedViewController: UIViewController, PTDBeanDelegate {
     
     
-
+    
     let refreshControl = UIRefreshControl()
-
+    
     var connectedBean: PTDBean?
+    var connectedBean2: PTDBean?
+    var connectedBean3: PTDBean?
     var currentTemperature: Temperature = Temperature(degreesCelcius: 0.0) {
         didSet {
             updateTemperatureView()
             updateBean()
         }
     }
-
+    
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var temperatureView: TemperatureView!
-
+    
     // MARK: Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        Alamofire.request(.GET, "http://152.23.11.115:3000/register").response { (request, response, data, error) in
+                print(request)
+                print(response)
+                print(data)
+                print(error)
+        }
+        
+        let comment: [String:AnyObject] = [
+            "comment": "my First Comment",
+            "commentDate": "2014-05-13 14:30 PM",
+            "isSigned": 1,
+            "patientId": 2,
+            "documentId": 3
+        ]
+        
+        Alamofire.request(.POST, "http://152.23.11.115:3000/register", parameters: comment).responseJSON { response in debugPrint(response)
+            print(response.request)
+            print(response.response)
+            print(response.data)
+            print(response.result)
+        }
+        
+        
         // Update the name label.
         temperatureView.nameLabel.text = connectedBean?.name
-
+        
         // Add pull-to-refresh control.
         refreshControl.addTarget(self, action: "didPullToRefresh:", forControlEvents: .ValueChanged)
         refreshControl.tintColor = .whiteColor()
         scrollView.addSubview(refreshControl)
     }
-
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         connectedBean?.readAccelerationAxes()
     }
-
+    
     // MARK: Actions
-
+    
     func didPullToRefresh(sender: AnyObject) {
         refreshControl.endRefreshing()
-        connectedBean?.readAccelerationAxes()
+        //connectedBean?.readAccelerationAxes()
     }
-
+    
     // MARK: PTDBeanDelegate
-
+    
     func bean(bean: PTDBean!, didUpdateTemperature degrees_celsius: NSNumber!) {
-    }
+        
 
+    }
+    
     func bean(bean: PTDBean!, didUpdateAccelerationAxes acceleration: PTDAcceleration){
         //print("x:%d y:%d z: %d", acceleration.x, acceleration.y, acceleration.z)
         temperatureView.temperatureLabel.text = String(format: "x:%d y:%d z: %d", acceleration.x, acceleration.y, acceleration.z)
     }
     
-   
+    
     /**************************************************************************************/
     func bean(bean: PTDBean!, serialDataReceived data: NSData!){
+        //  concurrencyQueue =
         var g = 0;
         /**
         * All accelerometer data/timesync messages are 32 / 8 or 4 bytes
@@ -111,9 +148,9 @@ class ConnectedViewController: UIViewController, PTDBeanDelegate {
             
             NSLog("Unhandled serial message: " + String(data))
             var hexString = String(data);
-         
-                var firstChar =  hexString.removeAtIndex(hexString.startIndex.advancedBy(1))
-                var secondChar = hexString.removeAtIndex(hexString.startIndex.advancedBy(2))
+            
+            var firstChar =  hexString.removeAtIndex(hexString.startIndex.advancedBy(1))
+            var secondChar = hexString.removeAtIndex(hexString.startIndex.advancedBy(2))
             
             
             
@@ -163,57 +200,57 @@ class ConnectedViewController: UIViewController, PTDBeanDelegate {
                 g += 15*1
             }
             
-                if (firstChar == "1"){
+            if (firstChar == "1"){
                 g += 1*16
-                }
-                if (firstChar == "2"){
+            }
+            if (firstChar == "2"){
                 g += 2*16
-                }
-                if (firstChar == "3"){
+            }
+            if (firstChar == "3"){
                 g += 3*16
-                }
-                if (firstChar == "4"){
+            }
+            if (firstChar == "4"){
                 g += 4*16
-                }
-                if (firstChar == "5"){
+            }
+            if (firstChar == "5"){
                 g += 5*16
-                }
-                if (firstChar == "6"){
+            }
+            if (firstChar == "6"){
                 g += 6*16
-                }
-                if (firstChar == "7"){
+            }
+            if (firstChar == "7"){
                 g += 7*16
-                }
-                if (firstChar == "8"){
+            }
+            if (firstChar == "8"){
                 g *= -1
-                }
-                if (firstChar == "9"){
-                    g += 16; g *= -1;
-                }
-                if (firstChar == "a"){
-                    g += 16*2; g *= -1;
-                }
-                if (firstChar == "b"){
-                    g += 16*3; g *= -1;
-                }
-                if (firstChar == "c"){
-                    g += 16*4; g *= -1;
-                }
-                if (firstChar == "d"){
-                    g += 16*5; g *= -1;
-                }
-                if (firstChar == "e"){
-                    g += 16*6; g *= -1;
-                }
-                if (firstChar == "f"){
-                    g += 16*7; g *= -1;
-                }
+            }
+            if (firstChar == "9"){
+                g += 16; g *= -1;
+            }
+            if (firstChar == "a"){
+                g += 16*2; g *= -1;
+            }
+            if (firstChar == "b"){
+                g += 16*3; g *= -1;
+            }
+            if (firstChar == "c"){
+                g += 16*4; g *= -1;
+            }
+            if (firstChar == "d"){
+                g += 16*5; g *= -1;
+            }
+            if (firstChar == "e"){
+                g += 16*6; g *= -1;
+            }
+            if (firstChar == "f"){
+                g += 16*7; g *= -1;
+            }
             
             
-           
             
             
-                //if the char is numeric, leave it, else turn it into the right integer and multiply
+            
+            //if the char is numeric, leave it, else turn it into the right integer and multiply
             
             
             temperatureView.temperatureLabel.text = String(format: "%i", g)
@@ -221,90 +258,46 @@ class ConnectedViewController: UIViewController, PTDBeanDelegate {
             
             return;
         }
-      
-            someBytes?.appendBytes(data.bytes, length: 1)
-            byteCounter++;
-            if (byteCounter == 4){
-                byteCounter = 0;
-                //now use Joshua's code on the array 'someBytes'
-              //  if ((someBytes. & 0x000000FF) >= 0x000000C0){
-                    /**
-                    * The following comments use left
-                    * to right bit numbering starting at 0. (bits 0-1 are signal)
-                    */
-                    // The X reading is bits 2-11. The 6 rightmost bits from byte
-                    // 0 and the 4 leftmost bits from byte 1.
-                    
-               //     var xbytes = NSData(bytes: [UInt8(someBytes[0]) & 0x3F >> 4,
-                        //UInt8(someBytes[1]) & 0xF0 >> 4] as [UInt8], length: 2)
-                    // The Y reading is bits 12-21. The 4 rightmost bits from byte
-                    // 1 and the 6 leftmost bits from byte 2.
-               //    var ybytes = NSData(bytes: [UInt8(someBytes[1]) & 0x0F >> 2,
-                 //       UInt8(someBytes[2]) & 0xFC >> 2] as [UInt8], length: 2)
-                    // The Z reading is bits 22-31. The 2 rightmost bits from byte
-                    // 2 and all the bits from from byte 3.
-                   // var zbytes = NSData(bytes: [UInt8(someBytes[2]) & 0x03,
-                     //   UInt8(someBytes[3])] as [UInt8], length: 2)
-                    
-                   // NSLog("%i, %i, %i", xbytes,ybytes,zbytes)
-                   // temperatureView.temperatureLabel.text = String(format: "x:%s y:%s z: %s", //String(xbytes),String(ybytes),String(zbytes))
-                    return;
-              //  }
-                // It's a timesync message
-                //TODO
-                return;
-            }
+        
+        someBytes?.appendBytes(data.bytes, length: 1)
+        byteCounter++;
+        if (byteCounter == 4){
+            byteCounter = 0;
+            return;
+            return;
+        }
         
     }
     
     
-
+    
     // MARK: Helper
-
+    
     func updateTemperatureView() {
-        // Update the temperature label.
-        //temperatureView.temperatureLabel.text = String(format: "%.f℉", currentTemperature.degressFahrenheit)
-
-        // Update the background color.
         var backgroundColor: UIColor
-
+        
         switch currentTemperature.state() {
-            case .Unknown:
-                backgroundColor = .blackColor()
-            case .Cold:
-                backgroundColor = .CBColdColor()
-            case .Cool:
-                backgroundColor = .CBCoolColor()
-            case .Warm:
-                backgroundColor = .CBWarmColor()
-            case .Hot:
-                backgroundColor = .CBHotColor()
+        case .Unknown:
+            backgroundColor = .blackColor()
+        case .Cold:
+            backgroundColor = .CBColdColor()
+        case .Cool:
+            backgroundColor = .CBCoolColor()
+        case .Warm:
+            backgroundColor = .CBWarmColor()
+        case .Hot:
+            backgroundColor = .CBHotColor()
         }
-
+        
         UIView.animateWithDuration(0.4, animations: {
             self.scrollView.backgroundColor = backgroundColor
             self.temperatureView.containerView.backgroundColor = backgroundColor
         })
     }
     
-  /*  func decodeBytes (bytes: NSData!) -> UInt8  {
-    /* Note on Java and Bitwise Operators
-    *  Java bitwise operators only work on ints and longs,
-    *  Bytes will undergo promotion with sign extension first.
-    *  So, we have to undo the sign extension on the lower order
-    *  bits here.
-    */
-    var ans = UInt8(someBytes[0]);
-    for var i = 1; i < someBytes.count; i++ {
-    ans <<= 8;
-    ans |= UInt8(someBytes[i]) & 0xFF;
-    }
-    return ans;
-    }
-*/
     func updateBean() {
-       // connectedBean?.setLedColor(temperatureView.containerView.backgroundColor)
+        // connectedBean?.setLedColor(temperatureView.containerView.backgroundColor)
     }
     
-   
+    
 }
--- iOS/Cool Beans/Controllers/DisconnectedViewController.swift
+++ iOS/Cool Beans/Controllers/DisconnectedViewController.swift
@@ -9,89 +9,139 @@
 import UIKit
 
 class DisconnectedViewController: UIViewController, PTDBeanManagerDelegate {
-
+    
+    var beans = NSMutableDictionary.init(capacity: 3);
+    
     let connectedViewControllerSegueIdentifier = "ViewConnection"
-
+    
     var manager: PTDBeanManager!
+    
     var connectedBean: PTDBean? {
         didSet {
-            if connectedBean == nil {
+            if  connectedBean == nil{
                 beanManagerDidUpdateState(manager)
             } else {
                 performSegueWithIdentifier(connectedViewControllerSegueIdentifier, sender: self)
             }
         }
     }
-
+    
+    var connectedBean2: PTDBean?{
+        didSet{
+            if connectedBean2 == nil {
+                beanManagerDidUpdateState(manager)
+            } else{
+                performSegueWithIdentifier(connectedViewControllerSegueIdentifier, sender: self)
+            }
+        }
+    }
+    
+    var connectedBean3: PTDBean?{
+        didSet{
+            if connectedBean2 == nil {
+                beanManagerDidUpdateState(manager)
+            } else{
+                performSegueWithIdentifier(connectedViewControllerSegueIdentifier, sender: self)
+            }
+        }
+    }
+    
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-
+    
     // MARK: Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         manager = PTDBeanManager(delegate: self)
     }
-
+    
     // MARK: Navigation
-
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == connectedViewControllerSegueIdentifier {
             let vc = segue.destinationViewController as! ConnectedViewController
             vc.connectedBean = connectedBean
+            vc.connectedBean2 = connectedBean2
+            vc.connectedBean3 = connectedBean3
             vc.connectedBean?.delegate = vc
         }
     }
-
+    
     // MARK: PTDBeanManagerDelegate
-
+    
     func beanManagerDidUpdateState(beanManager: PTDBeanManager!) {
         switch beanManager.state {
-            case .Unsupported:
-                UIAlertView(
-                    title: "Error",
-                    message: "This device is unsupported.",
-                    delegate: self,
-                    cancelButtonTitle: "OK"
-                    ).show()
-            case .PoweredOff:
-                UIAlertView(
-                    title: "Error",
-                    message: "Please turn on Bluetooth.",
-                    delegate: self,
-                    cancelButtonTitle: "OK"
-                    ).show()
-            case .PoweredOn:
-                beanManager.startScanningForBeans_error(nil);
-            default:
-                break
+        case .Unsupported:
+            UIAlertView(
+                title: "Error",
+                message: "This device is unsupported.",
+                delegate: self,
+                cancelButtonTitle: "OK"
+                ).show()
+        case .PoweredOff:
+            UIAlertView(
+                title: "Error",
+                message: "Please turn on Bluetooth.",
+                delegate: self,
+                cancelButtonTitle: "OK"
+                ).show()
+        case .PoweredOn:
+            beanManager.startScanningForBeans_error(nil);
+        default:
+            break
         }
     }
-
+    /**************************************************************************************************/
     func beanManager(beanManager: PTDBeanManager!, didDiscoverBean bean: PTDBean!, error: NSError!) {
+        let key = bean.identifier
+        if ((self.beans.objectForKey(key)) == nil){
+            //new bean
+            beans.setObject(bean, forKey: key)
+        }
         print("DISCOVERED BEAN \nName: \(bean.name), UUID: \(bean.identifier) RSSI: \(bean.RSSI)")
         if connectedBean == nil {
             if bean.state == .Discovered {
                 manager.connectToBean(bean, error: nil)
             }
         }
+        if connectedBean2 == nil {
+            if bean.state == .Discovered {
+                manager.connectToBean(bean, error: nil)
+            }
+        }
+        if connectedBean3 == nil {
+            if bean.state == .Discovered {
+                manager.connectToBean(bean, error: nil)
+            }
+        }
+        
     }
-
+    /**************************************************************************************************/
     func BeanManager(beanManager: PTDBeanManager!, didConnectToBean bean: PTDBean!, error: NSError!) {
         print("CONNECTED BEAN \nName: \(bean.name), UUID: \(bean.identifier) RSSI: \(bean.RSSI)")
         if connectedBean == nil {
             connectedBean = bean
+            return;
+        }
+        if connectedBean2 == nil {
+            connectedBean2 = bean
+            return;
+        }
+        if connectedBean3 == nil{
+            connectedBean3 = bean
+            return;
         }
     }
-
+    
     func beanManager(beanManager: PTDBeanManager!, didDisconnectBean bean: PTDBean!, error: NSError!) {
         print("DISCONNECTED BEAN \nName: \(bean.name), UUID: \(bean.identifier) RSSI: \(bean.RSSI)")
-
+        
         // Dismiss any modal view controllers.
         presentedViewController?.dismissViewControllerAnimated(true, completion: {
             self.dismissViewControllerAnimated(true, completion: nil)
         })
-
+        
         self.connectedBean = nil
     }
-
- }
+    
+}
